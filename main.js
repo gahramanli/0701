b944f7a53402315a1c52edb04337fc6a7365fe1e
gahramanli <116644249+gahramanli@users.noreply.github.com>
2023-01-07 20:22:29 +0400

    Task-1

--- main.js
+++ main.js
@@ -1,20 +1,9 @@
-let count=0;
 
-document.getElementById('btn').onclick=function(){
-    
-    count++;
 
-        if (count%2 != 0) {
-            document.getElementById('box').style.width="100px";
-            document.getElementById('box').style.height="100px";
-            document.getElementById('box').style.backgroundColor="black";
-            document.getElementById('box').style.display="flex";
-        }
-        else{
-            document.getElementById('box').style.display="none";
+document.getElementById('box').onclick=function(){
+    document.getElementById('box').style.display="none";
+}
     
-        }
-    }
-    
+
 
 
